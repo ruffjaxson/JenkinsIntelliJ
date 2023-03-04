a1687391a5348f914e140944b108f83b6e8669d0
Jaxson Ruff <ruffjaxson@gmail.com>
2023-03-04 12:01:17 -0700

    Added a tip

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -49,6 +49,7 @@ class Calculator {
     if int a = 0 then this method returns: 0
     if int a = 10 then this method returns: 1010
     if int a = 16 then this method returns: 10000
+    Tip: use “Integer.toBinaryString(n)”
      */
     String intToBinaryNumber(int n){
         return null;
