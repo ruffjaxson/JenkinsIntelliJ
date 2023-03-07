61956955ae0d3600fdb4bdae2338bb3fec949e8b
Jaxson Ruff <ruffjaxson@gmail.com>
2023-03-06 20:18:14 -0700

    Added terminal capability

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.UUID;
 
 class Calculator {
@@ -5,6 +6,43 @@ class Calculator {
 
     Calculator(){
 
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            String[] tokens = input.split(" ");
+
+            if (tokens[0].equals("add")) {
+                int a = Integer.parseInt(tokens[1]);
+                int b = Integer.parseInt(tokens[2]);
+                System.out.println(calculator.add(a, b));
+            } else if (tokens[0].equals("subtract")) {
+                int a = Integer.parseInt(tokens[1]);
+                int b = Integer.parseInt(tokens[2]);
+                System.out.println(calculator.subtract(a, b));
+            } else if (tokens[0].equals("multiply")) {
+                int a = Integer.parseInt(tokens[1]);
+                int b = Integer.parseInt(tokens[2]);
+                System.out.println(calculator.multiply(a, b));
+            } else if (tokens[0].equals("divide")) {
+                int a = Integer.parseInt(tokens[1]);
+                int b = Integer.parseInt(tokens[2]);
+                System.out.println(calculator.divide(a, b));
+            } else if (tokens[0].equals("fibonacci")) {
+                int n = Integer.parseInt(tokens[1]);
+                System.out.println(calculator.fibonacciNumberFinder(n));
+            } else if (tokens[0].equals("binary")) {
+                int n = Integer.parseInt(tokens[1]);
+                System.out.println(calculator.intToBinaryNumber(n));
+            } else if (tokens[0].equals("quit")) {
+                break;
+            }
+        }
+        scanner.close();
     }
 
     int add(int a , int b){
@@ -53,7 +91,6 @@ class Calculator {
             fibNum += prevFibNum;
             prevFibNum = temp;
         }
-        gonna break
 
         return fibNum;
     }
